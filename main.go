b05fe6298f63f01d2d973a148aab15d67e4ba57a
christian <christian@localhost.localdomain>
2020-04-04 15:44:06 -0400

    add urls

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-import(
-	"net/http"
+import (
+	"fmt"
 	"log"
+	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hola Mundo")
+	})
+
+	http.HandleFunc("/dos", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hola Mundo 2")
+	})
+
 	log.Fatal(http.ListenAndServe("localhost:3000", nil))
 }
