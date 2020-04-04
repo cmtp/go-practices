3979417f942d7ed100d61207dd8bcd5ea80e1e7b
christian <christian@localhost.localdomain>
2020-04-04 16:09:04 -0400

    Envio de errores

--- main.go
+++ main.go
@@ -15,7 +15,8 @@ func main() {
 
 	http.HandleFunc("/dos", func(w http.ResponseWriter, r *http.Request) {
 		// fmt.Fprintf(w, "Hola Mundo 2")
-		http.NotFound(w, r)
+		// http.NotFound(w, r)
+		http.Error(w, "Este es un error.", http.StatusConflict)
 	})
 
 	log.Fatal(http.ListenAndServe("localhost:3000", nil))
