50e558f6f4f6a6800ca2fd0df6eb6b1e2a2080c3
christian <christian@localhost.localdomain>
2020-04-06 16:43:36 -0400

    New Server Mux

--- server/main.go
+++ server/main.go
@@ -11,12 +11,18 @@ func Hola(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
 }
 
+func Hola2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World 2")
+}
+
 func main() {
-	http.HandleFunc("/", Hola) // DefaultServerMux
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Hola)
+	mux.HandleFunc("/dos", Hola2)
 
 	server := &http.Server{
 		Addr:    "localhost:3000",
-		Handler: nil,
+		Handler: mux,
 	}
 
 	log.Fatal(server.ListenAndServe())
