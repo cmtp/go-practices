489fc041fc605b3075c4987510b40e276f0ba3cc
christian <christian@localhost.localdomain>
2020-04-08 10:35:00 -0400

    Use gorillamux parameters

---
+++ gorilla.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	nombre := vars["nombre"]
+	id := vars["id"]
+
+	fmt.Fprintf(w, "Los parametros son "+nombre+" "+id)
+	// w.Write([]byte("Gorilla!\n"))
+}
+
+func main() {
+	r := mux.NewRouter()
+	// Routes consist of a path and a handler function.
+	r.HandleFunc("/usuarios/{nombre}/{id:[0-9]+}", YourHandler)
+
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe(":3000", r))
+}
