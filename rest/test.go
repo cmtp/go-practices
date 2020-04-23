494170bd40b62f49f10b68d8e5ac8b44a2f56ae4
cmtp <christian.tola@hotmail.com>
2020-04-23 10:29:39 -0400

    add Update function

--- rest/test.go
+++ rest/test.go
@@ -11,5 +11,10 @@ func main() {
 
 	user := models.CreateUser("christian", "123", "christian@admin.com")
 	fmt.Println(user)
+
+	user.Username = "Cambio de nombre"
+	user.Password = "Cambio de Password"
+	user.Email = "Cambio de Email"
+	user.Save()
 	models.CloseConnection()
 }
