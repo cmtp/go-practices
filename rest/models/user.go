3b0e38342b04e59debc129f5ff88223f988bede2
ctola <christian.tola@truextend.com>
2020-04-22 20:23:01 -0400

    Save User on database

--- rest/models/user.go
+++ rest/models/user.go
@@ -1,6 +1,6 @@
 package models
 
-import "errors"
+// import "errors"
 
 type User struct {
 	ID       int    `json:"id"`
@@ -15,45 +15,14 @@ const userSchema string = `CREATE TABLE users(
 		email VARCHAR(40),
 		created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`
 
-
-
 type Users []User
 
-var users = make(map[int]User)
-
-func SetDefaultUser() {
-	user := User{ID: 1, Username: "chris", Password: "admin123"}
-	users[user.ID] = user
-}
-
-func GetUsers() Users {
-	list := Users{}
-	for _, user := range users {
-		list = append(list, user)
-	}
-	return list
-}
-
-func GetUser(userId int) (User, error) {
-	if user, ok := users[userId]; ok {
-		return user, nil
-	}
-	return User{}, errors.New("El usuario no se encuentra dentro del mapa")
-}
-
-func SaveUser(user User) User {
-	user.ID = len(users) + 1
-	users[user.ID] = user
+func NewUser(username, password string) *User {
+	user := &User { Username: username, Password: password}
 	return user
 }
 
-func UpdateUser(user User, username, password string) User {
-	user.Username = username
-	user.Password = password
-	users[user.ID] = user
-	return user
-}
-
-func DeleteUser(id int) {
-	delete(users, id)
+func (this *User) Save() {
+	sql := "INSERT users SET username=?, password=?"
+	Exec(sql, this.Username, this.Password)
 }
--- rest/test.go
+++ rest/test.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"./models"
 )
 
 func main() {
 	models.CreateConnection()
 	models.CreateTables()
+
+	user := models.NewUser("christian", "123")
+	user.Save()
+	fmt.Println(user)
 	models.CloseConnection()
 }
